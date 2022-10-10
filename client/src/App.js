0f8bdd0d1d19a81167ebcb86cc84ccce0f03cbd1
moezthemaster <you@example.com>
2022-10-10 16:39:37 +0000

    fix some issues

--- client/src/App.js
+++ client/src/App.js
@@ -17,9 +17,8 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Fib Calculator
           </a>
-	  <p>Version 1.0.0</p>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
